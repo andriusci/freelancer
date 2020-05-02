f3d816a36e491c10a11c666640e67383c28806a2
Andrius <>
2020-05-02 13:20:49 +0100

    if else for database url

--- freelancer_website/settings.py
+++ freelancer_website/settings.py
@@ -80,17 +80,21 @@ WSGI_APPLICATION = 'freelancer_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-  #  }
-#}
-
 DATABASES = {
-   'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
+if "DATABASE_URL" in os.environ:
+   DATABASES = {
+      'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+   DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 
 
 # Password validation
