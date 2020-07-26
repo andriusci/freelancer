a447a23b4b8033a6446b48db83e17917dc01221a
Andrius <>
2020-07-26 12:39:35 +0100

    never ending buging

--- user_accounts/views.py
+++ user_accounts/views.py
@@ -120,7 +120,7 @@ def user_login(request):
                 loginForm.add_error(None, "Your username or password is incorrect")
     else:
        if request.user.is_authenticated:
-          return render(request, 'index.html')
+          return redirect('user_account')
        else:
           loginForm = LoginForm()
           return render(request, 'login.html', {"loginForm": loginForm})
