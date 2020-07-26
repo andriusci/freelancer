271f9b97b97aeff48592482f2765d7dd853a4699
Andrius <>
2020-07-26 19:52:34 +0100

    chat update

--- chat/views.py
+++ chat/views.py
@@ -35,6 +35,8 @@ def chat(request, quote_ref, file_name):
 
 def chat_send(request):
   if request.method == "POST":
+      current_user = request.user
+      user = current_user.username
       form = ChatForm(request.POST)
       if form.is_valid:
            message = request.POST.get('message')
