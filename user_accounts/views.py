aeb513644a7e27bfb01f0a0d59c5761f4280b047
Andrius <>
2020-07-29 12:45:52 +0100

    aaa

--- user_accounts/views.py
+++ user_accounts/views.py
@@ -147,10 +147,7 @@ def user_login(request):
                 user_form.add_error(None, "Your username or password are incorrect")
     else:
         user_form = LoginForm()
-
-    args = {'user_form': user_form, 'next': request.GET.get('next', '')}
-    return render(request, 'login.html', args)
-
+        return render(request, 'login.html', {"loginForm": loginForm})
 
 def signup(request):
     #Creates new user
